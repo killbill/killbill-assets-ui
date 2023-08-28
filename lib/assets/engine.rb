aeb1c856fefc748087d69d91d767d1d9a619b074
Kyle Le <tungleduy.xyz@gmail.com>
2023-08-28 20:08:42 +0700

    Add assets to the gem

--- lib/assets/engine.rb
+++ lib/assets/engine.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
-
+require 'font-awesome-rails'
+require 'jquery-rails'
+require 'jquery-datatables-rails'
+require 'bootstrap-sass'
+require 'bootstrap-datepicker-rails'
 module Assets
   class Engine < ::Rails::Engine
     isolate_namespace Assets
