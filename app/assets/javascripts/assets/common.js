ec48e936a30555f8e7120f06c6aefdb2e742691d
Kyle Le <tungleduy.xyz@gmail.com>
2023-08-31 00:07:58 +0700

    Add missing JS bootstrap-sprockets

--- app/assets/javascripts/assets/common.js
+++ app/assets/javascripts/assets/common.js
@@ -7,5 +7,6 @@
 //= require_tree .
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
