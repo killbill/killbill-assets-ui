d925c2b247e1089084d957629b887e47b3a5978a
Kyle Le <tungleduy.xyz@gmail.com>
2023-09-20 22:01:35 +0700

    Remove SASS dependencies

--- lib/assets/engine.rb
+++ lib/assets/engine.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 require 'font-awesome-rails'
 require 'jquery-rails'
-require 'jquery-datatables-rails'
-require 'bootstrap-sass'
 require 'bootstrap-datepicker-rails'
 module Assets
   class Engine < ::Rails::Engine
